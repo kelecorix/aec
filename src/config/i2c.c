1e315d76d814ed52cd93f58ab8ced378b7dc3dcb
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-09 07:00:11 +0300

    Add i2c stubs

---
+++ src/config/i2c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+
---
+++ src/hw/i2c.h
@@ -0,0 +1,14 @@
+#ifndef I2C_H_
+#define I2C_H_
+
+#include <stdio.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <string.h>
+
+
+#endif /* I2C_H_ */
