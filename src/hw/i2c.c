2392fe77fed37e9ad65e59553de692c0bb399bec
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-10 13:11:52 +0300

    Debug

--- src/hw/i2c.c
+++ src/hw/i2c.c
@@ -76,7 +76,7 @@ void i2cTestHardware() {
   set_i2c_register(g_i2cFile, addrFan2, 0, steps[5]);
   sleep(5);
   set_i2c_register(g_i2cFile, addrFan1, 0, steps[0]);
-  set_i2c_register(g_i2cFile, addrFan2, 0, steps[5]);
+  set_i2c_register(g_i2cFile, addrFan2, 0, steps[0]);
 
   i2cClose();
 
