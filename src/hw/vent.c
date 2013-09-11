ed14948fad23e4acb17390d46d9320ef62d9f609
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-11 19:52:36 +0300

    Debug

--- src/hw/vent.c
+++ src/hw/vent.c
@@ -52,7 +52,8 @@ int set_turns(Vent* vent, int val) {
       addr = strtol(getStr(site->cfg, (void *) "a_vent_in"), NULL, 16);
     else
       addr = strtol(getStr(site->cfg, (void *) "a_vent_out"), NULL, 16);
-    printf("Управляем регистром, адрес %d, значение %d\n", addr, steps[val]);
+    printf("Управляем регистром, адрес %d, значение %d, %d \n", addr, val, steps[val]);
+    i2cSetAddress(addr);
     set_i2c_register(g_i2cFile, addr, 0, steps[val]);
     vent->turns = val;
     return 1;
