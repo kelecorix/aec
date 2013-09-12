07494c5bb65a8ef576a18754fbbdb1729c26ee09
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-12 15:43:33 +0300

    Debug

--- src/hw/ac.c
+++ src/hw/ac.c
@@ -98,7 +98,7 @@ static int set_mode(AC* ac, int val) {
     else
       value &= ~(1 << bit) ; // очистим бит
 
-    printf("Управляем регистром, адрес %x, значение %d, %x \n", addr, val, value);
+    printf("Управляем регистром, адрес %x, значение %d, %x , бит %d \n", addr, val, value, bit);
     set_i2c_register(g_i2cFile, addr, value, value);
     ac->mode = val;
     i2cClose();
