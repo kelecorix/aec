36744cba839b477c995731cdd802a9fdfd4e936d
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-13 11:13:01 +0300

    Correct code style

--- src/hw/throttle.c
+++ src/hw/throttle.c
@@ -5,7 +5,8 @@
 #include "i2c.h"
 #include "site.h"
 
-static int steps[11] = {0xFF, 0xED, 0xDF, 0xDE, 0xDC, 0xBF, 0xBE, 0x7F, 0x7E, 0x9F, 0x8F};
+static int steps[11] = { 0xFF, 0xED, 0xDF, 0xDE, 0xDC, 0xBF, 0xBE, 0x7F, 0x7E,
+    0x9F, 0x8F };
 
 /*
  * Изменим режим заслонки 
@@ -20,7 +21,8 @@ static int set_mode(Throttle* th, int val) {
       value = 10; // максимальное значение
     else
       value = 0; // 1 минимальное значение вроде как 0
-    printf("Заслонка set_mode, адрес %x, значение %d, %x , steps %x\n", addr, val, value, steps[value]);
+    printf("Заслонка set_mode, адрес %x, значение %d, %x , steps %x\n", addr,
+        val, value, steps[value]);
     set_i2c_register(g_i2cFile, addr, steps[value], steps[value]);
     th->position = val;
     i2cClose();
@@ -35,14 +37,15 @@ static int set_mode(Throttle* th, int val) {
 int set_position(Throttle* th, int val) {
 
   int i;
-  for(i=0;i<11;i++) {
+  for (i = 0; i < 11; i++) {
     printf("set_position test значение %d, 0x%x\n", i, steps[i]);
   }
   i2cOpen();
   if (val >= 0 && val <= 10) {
     int addr;
     addr = strtol(getStr(site->cfg, (void *) "a_throttle"), NULL, 16);
-    printf("Заслонка set_position, адрес %x, значение %d, %d\n", addr, val, steps[val]);
+    printf("Заслонка set_position, адрес %x, значение %d, %d\n", addr, val,
+        steps[val]);
     set_i2c_register(g_i2cFile, addr, steps[val], steps[val]);
     th->position = val;
     i2cClose();
--- src/hw/vent.c
+++ src/hw/vent.c
@@ -5,8 +5,8 @@
 #include "i2c.h"
 #include "site.h"
 
-static int steps[11] = { 0xFF, 0xED, 0xDF, 0xDE, 0xDC, 0xBF, 0xBE, 0x7F, 0x7E, 0x9F,
-     0x8F};
+static int steps[11] = { 0xFF, 0xED, 0xDF, 0xDE, 0xDC, 0xBF, 0xBE, 0x7F, 0x7E,
+    0x9F, 0x8F };
 
 void vent_free() {
   //TODO: очистим ресурсы памяти
@@ -59,7 +59,8 @@ int set_turns(Vent* vent, int val) {
       addr = strtol(getStr(site->cfg, (void *) "a_vent_in"), NULL, 16);
     else
       addr = strtol(getStr(site->cfg, (void *) "a_vent_out"), NULL, 16);
-    printf("Управляем регистром, адрес %d, значение %d, %d \n", addr, val, steps[val]);
+    printf("Управляем регистром, адрес %d, значение %d, %d \n", addr, val,
+        steps[val]);
     i2cSetAddress(addr);
     set_i2c_register(g_i2cFile, addr, 0, steps[val]);
     vent->turns = val;
