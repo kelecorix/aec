ff5a2161a9d07373476c7fd13098a28931d1c6cf
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-14 15:40:33 +0300

    Debug

--- src/hw/i2c.c
+++ src/hw/i2c.c
@@ -172,7 +172,7 @@ void i2cTestHardware() {
 //    site->vents[0]->set_turns(site->vents[0], i);
 //    site->vents[1]->set_turns(site->vents[1], i);
 //
-    sleep(15);
+    sleep(20);
 //
 //    tacho1 = i2c_get_tacho_data(site->vents[0], strtol(a_tacho_in, NULL, 16));
 //    tacho2 = i2c_get_tacho_data(site->vents[1], strtol(a_tacho_out, NULL, 16));
