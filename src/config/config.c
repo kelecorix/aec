f71cbd9da905c22049aa2e3b41a02b4525c4fd61
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-14 11:57:42 +0300

    Debug

--- src/config/config.c
+++ src/config/config.c
@@ -9,9 +9,9 @@
 
 ConfigTable* config_table_new() {
   ConfigTable* cfg = malloc(sizeof(ConfigTable));
-  cfg->mTable = hashmapCreate(32, str_hash_fn, hashmapIntEquals);
-  cfg->mStatic = hashmapCreate(32, str_hash_fn, hashmapIntEquals);
-  cfg->mOptional = hashmapCreate(32, str_hash_fn, hashmapIntEquals);
+  cfg->mTable = hashmapCreate(34, str_hash_fn, hashmapIntEquals);
+  cfg->mStatic = hashmapCreate(34, str_hash_fn, hashmapIntEquals);
+  cfg->mOptional = hashmapCreate(34, str_hash_fn, hashmapIntEquals);
   return cfg;
 }
 
