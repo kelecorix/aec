2ddc96da99d09e04ad60dc6641c3e922ae70f862
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-18 11:58:43 +0300

    Small correct

--- src/hw/vent.c
+++ src/hw/vent.c
@@ -12,13 +12,13 @@ static int steps[11] = { 0xFF, 0xF8, 0xF7, 0xF6, 0xF3, 0xEE, 0xEC, 0xE6, 0xDC,
     0xD3, 0x00 };
 
 // Таблицы преобразования обороты в шаг
-static int tts1[11][2] = { { 120, 130 }, { 119, 120 }, { 410, 420 },
-    { 620, 640 }, { 780, 810 }, { 780, 810 }, { 910, 930 }, { 990, 1100 }, {
-        1190, 1210 }, { 1320, 1340 }, { 1490, 1510 } };
+static int tts1[11][2] = { { 70, 120 }, { 240, 300 }, { 400, 480 },
+    { 520, 600 }, { 750, 820 }, { 890, 950 }, { 980, 1020 }, { 1050, 1120 }, {
+        1300, 1360 }, { 1300, 1520 }, { 1300, 1520 } };
 
-static int tts2[11][2] = { { 120, 130 }, { 290, 315 }, { 490, 510 },
-    { 621, 641 }, { 655, 675 }, { 740, 760 }, { 740, 760 }, { 780, 810 }, { 840,
-        860 }, { 910, 930 }, { 910, 1100 } };
+static int tts2[11][2] = { { 70, 120 }, { 300, 350 }, { 420, 480 },
+    { 550, 590 }, { 610, 650 }, { 730, 770 }, { 780, 820 }, { 830, 870 }, { 830,
+        870 }, { 900, 1100 }, { 900, 1100 } };
 
 void vent_free() {
   //TODO: очистим ресурсы памяти
@@ -165,7 +165,7 @@ int turns_to_step(int turns, int type) {
 
 void test_vents() {
 
-  int i, step, tacho1, tacho2, th_r;
+  int i, step, tacho1, tacho2, step1, step2;
   char *buf;
 
   char *a_tacho_in = getStr(site->cfg, (void *) "a_tacho_flow_in");
@@ -181,8 +181,11 @@ void test_vents() {
     tacho1 = i2c_get_tacho_data(site->vents[0], strtol(a_tacho_in, NULL, 16));
     tacho2 = i2c_get_tacho_data(site->vents[1], strtol(a_tacho_out, NULL, 16));
 
-    printf("Шаг %d: tахо1 %d, tахо2 %d, \n", i, tacho1, tacho2);
+    step1 =  turns_to_step(tacho1, site->vents[0]->type);
+    step2 = turns_to_step(tacho2, site->vents[1]->type);
 
+    printf("Шаг %d: tахо1 %d, tахо2 %d, \n", i, tacho1, tacho2);
+    printf("Шаг %d: vent1 %d, vent2 %d\n", i, step1, step2);
   }
 
 }
