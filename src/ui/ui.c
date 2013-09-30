8aafd3d5c5e52846c0ee44ad81d75547a4a2bef2
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-30 11:39:12 +0300

    Debug

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -12,35 +12,35 @@ int dmode; // Мы либо в режиме редактирования 1, ли
 
 void run_ui(Site* site) {
 
-  // инициализируем экран
-  int addr_lcd = strtol(getStr(site->cfg, (void *) "a_lcd"), (char **) NULL, 16);
-  LCD* lcd = lcd_new(addr_lcd);
-  lcd->init(lcd);
-  lcd->reset(lcd);
-
-  // инициализируем клавиатуру
-  int addr_kb = strtol(getStr(site->cfg, (void *) "a_keyb"), (char **) NULL, 16);
-  KB* kb = kb_new(addr_kb);
-
-  // инициализируем меню
-  //init_menu();
-
-  // установим обработчик нажатия
-  //setHandler(MSG_MENU_SELECT, &selectMenu);
-
-  sleep(1);
-
-  dmode = 0;
-  while (1) {
-
-  // отслеживаем переход в меню
-
-  // выводим на экран показатели датчиков
-
-
-
-
-  }
+//  // инициализируем экран
+//  int addr_lcd = strtol(getStr(site->cfg, (void *) "a_lcd"), (char **) NULL, 16);
+//  LCD* lcd = lcd_new(addr_lcd);
+//  lcd->init(lcd);
+//  lcd->reset(lcd);
+//
+//  // инициализируем клавиатуру
+//  int addr_kb = strtol(getStr(site->cfg, (void *) "a_keyb"), (char **) NULL, 16);
+//  KB* kb = kb_new(addr_kb);
+//
+//  // инициализируем меню
+//  //init_menu();
+//
+//  // установим обработчик нажатия
+//  //setHandler(MSG_MENU_SELECT, &selectMenu);
+//
+//  sleep(1);
+//
+//  dmode = 0;
+//  while (1) {
+//
+//  // отслеживаем переход в меню
+//
+//  // выводим на экран показатели датчиков
+//
+//
+//
+//
+//  }
 }
 
 
