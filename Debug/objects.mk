9bbbd411a0b4443d002071921cd2fa1df21c29b3
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-09-30 11:11:01 +0300

    Correct makefiles

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lownet -lpthread -lowcapi
+LIBS :=
 
--- Debug/src/ui/subdir.mk
+++ Debug/src/ui/subdir.mk
@@ -4,21 +4,24 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../src/ui/buttons.c \
 ../src/ui/keyboard.c \
 ../src/ui/lcd.c \
+../src/ui/menu.c \
+../src/ui/msg.c \
 ../src/ui/ui.c 
 
 OBJS += \
-./src/ui/buttons.o \
 ./src/ui/keyboard.o \
 ./src/ui/lcd.o \
+./src/ui/menu.o \
+./src/ui/msg.o \
 ./src/ui/ui.o 
 
 C_DEPS += \
-./src/ui/buttons.d \
 ./src/ui/keyboard.d \
 ./src/ui/lcd.d \
+./src/ui/menu.d \
+./src/ui/msg.d \
 ./src/ui/ui.d 
 
 
@@ -26,7 +29,7 @@ C_DEPS += \
 src/ui/%.o: ../src/ui/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I/opt/owfs/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
