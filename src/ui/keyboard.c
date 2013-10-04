e382999ed2339e78b1f05ceccd91e222c01d8e9d
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2013-10-04 11:09:58 +0300

    Debug

--- src/ui/keyboard.c
+++ src/ui/keyboard.c
@@ -17,6 +17,7 @@ KB* kb_new(int addr) {
 }
 
 void reset_kb(KB* kb){
+  printf("врезете\n");
 
   if ((kb->fd = open(I2C_FILE_NAME, O_RDWR)) < 0) {
     log_1("Failed to open the i2c bus\n");
@@ -24,6 +25,7 @@ void reset_kb(KB* kb){
   } else {
     kb->connect = 1;
   }
+  printf("врезете2\n");
   if (ioctl(kb->fd, I2C_SLAVE, kb->address) < 0) {
     log_1("Failed to acquire bus access and/or talk to slave.\n");
     kb->connect = 0;
