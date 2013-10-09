54eabab2c0ee78be967183956873019f66eefa3c
root <root@UVO01000003.(none)>
2013-10-09 10:54:13 +0300

    Debug

--- scripts/freecooling2.sh
+++ scripts/freecooling2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 while true
 do
+cd /opt/owfs/freecooling/
 /opt/owfs/freecooling/freecooling2 -d 4 -p 0
+echo "sigfolt" >> /var/log/freecooling2_sh.log
 sleep 5
 done
 
---
+++ scripts/tmp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$d = `date`
+echo $d
